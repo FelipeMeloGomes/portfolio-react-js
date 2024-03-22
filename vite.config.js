83dd4b4f1c2d1e405ed98b192cdf609c1ab09efe
Felipe Melo <felipemelogomes36@gmail.com>
2024-03-21 22:47:43 -0300

    feat: add viteImagemin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    build: {
+        cssCodeSplit: false,
+        minify: true,
+        treeshake: true,
+        optimizeDeps: {
+            include: ["linked-dep"],
+        },
+        commonjsOptions: {
+            include: [/linked-dep/, /node_modules/],
+        },
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    }
+                },
+            },
+        },
+    },
+    plugins: [react(), viteImagemin()],
 });
